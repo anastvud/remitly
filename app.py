1579b6775ea7bb690a4044acd26f44c6e40bb7b0
anastvud <anastasiiavudvud@gmail.com>
2024-04-04 11:55:34 +0200

    .sh to run

--- app.py
+++ app.py
@@ -4,35 +4,33 @@ import os
 def verify(json_file):
 
     try:
-        f = open(json_file, 'r')
+        with open(json_file, 'r') as f:
+            file_size = os.path.getsize(json_file)
+            if (file_size == 0):
+                print("File is empty")
+                return True
+            try:
+                data = json.load(f)
+            except json.JSONDecodeError:
+                print("Invalid json file")
+                return True
     except FileNotFoundError:
         print(f"Could not read the file {json_file}")
         return True
-
-    with f:
-        file_size = os.path.getsize(json_file)
-        if (file_size == 0):
-            print("File is empty")
-            return True
-        try:
-            data = json.load(f)
-        except json.JSONDecodeError:
-            print("Invalid json file")
-            return True
-        
 
     resource = None
     try:
         resource = data['PolicyDocument']['Statement'][0]['Resource']
     except KeyError:
         print('Some key in .json file is missing')
-    
 
     if resource == '*':
         return False
     else:
         return True
+    
+root = os.getenv('REPO_ROOT')
 
-json_file = '/home/nastia/remitly/test.json'
+json_file = root + '/test.json'
 result = verify(json_file)
 print(result)
---
+++ source.sh
@@ -0,0 +1,3 @@
+export REPO_ROOT=$(git rev-parse --show-toplevel)
+
+python3 $REPO_ROOT/app.py
